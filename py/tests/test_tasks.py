dc0e02331297f63084e36d08adf1c7e90be3b66f
Rusty-Beard <tapinov.oleg@yandex.ru>
2018-10-12 22:18:25 +0300

    Fix #13 Add unit-tests

---
+++ py/tests/test_tasks.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest.mock import patch
+import ctl.tasks
+from app import app
+
+class TasksViewTestCase(unittest.TestCase):
+    @patch('ctl.tasks.flask')
+    @patch('dao.utils.mysql')
+    def test_connect_to_database(self, mysql_connector, flask):
+        with app.app_context():
+            mysql_connector.connect().cursor().fetchone.return_value = None
+            ctl.tasks.task_view('')
+            self.assertTrue(flask.abort.called)
+if __name__ == '__main__':
+    unittest.main()
